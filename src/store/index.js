fee0ed44c8bd19c02664d781bf4b7987a5f800d9
ASDW51 <1598926913@qq.com>
2022-09-01 11:01:50 +0800

    auto import store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,17 @@
 import Vue from "vue"
 import Vuex from "vuex"
 
+const path = require("path")
+
 Vue.use(Vuex)
+//自动装载modules
+const fileList = require.context("./modules",true,/\.js$/)
+let modules = {}
+fileList.keys().forEach(item=>{
+	let mod = require("./modules/" + path.join(item)).default
+	let modName = item.replace(/\.\/(.*)\/.*\w+$/,'$1')
+	modules[modName] = mod
+})
 
 export default new Vuex.Store({
 	state: {
@@ -22,5 +32,5 @@ export default new Vuex.Store({
 			bool?context.commit("setRequestCount",1):context.commit("setRequestCount",-1)
 		}
 	},
-	modules: {},
+	modules,
 })
